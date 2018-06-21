8aa1a51e8d5badaa1f15ded025ce86968d570956
Frederik J. Simons <fjsimons@gmail.com>
2018-06-20 23:25:29 -0400

    Computes a commonly used quadratic form

--- hformos.m
+++ hformos.m
@@ -21,7 +21,7 @@ function Xk=hformos(S,Hk,A,xver)
 %
 % In our formalism, this should result in a real-valued function. 
 %
-% Last modified by fjsimons-at-alum.mit.edu, 11/15/2016
+% Last modified by fjsimons-at-alum.mit.edu, 08/23/2017
 
 defval('A',1)
 defval('xver',1)
@@ -32,6 +32,7 @@ if size(A,2)==3 && size(Hk,2)==2
   Xk=Xk+1./S.*[2*real(conj(Hk(:,2)).*A(:,2).*Hk(:,1))+...
 		 conj(Hk(:,2)).*A(:,3).*Hk(:,2)];
 end
+
 % The 2*real is a shortcut to write the sum of the cross terms which
 % should cancel any imaginary parts... it's a magnitude after all
 % Still may have a tiny imaginary part, get rid of it if it is small 
