db97e0993e01f0bda6c4b4b20d5e1a8e9c6b06ca
Frederik J. Simons <fjsimons@gmail.com>
2018-10-31 12:32:32 -0400

    Slow, approximate blurring of a spectral matrix

--- bluros.m
+++ bluros.m
@@ -3,8 +3,8 @@ function [Sbar,k]=bluros(S,params,xver)
 %
 % Spectral blurring with periodogram of a boxcar. If we're talking about a
 % matrix, there are three columns, and there can be an eigenvalue check.
-% This is the approximate, slower, convolutional way which requires a "grid
-% refinement" parameter. Later, will build in other types of windows.
+% This is the approximate, slower, convolutional way which requires
+% an integer "grid refinement" parameter. Later, will build in other types of windows.
 %
 % INPUT:
 %
@@ -30,17 +30,11 @@ function [Sbar,k]=bluros(S,params,xver)
 %
 % SEE ALSO:
 %
-% SIMULOSL, BLUROSY
-%
-% NOTES:
-%
-% It looks to me that keeping blurs to an ODD number performs the best from
-% either even- or odd-sized inputs, despite the parity fix effectuated in
-% KNUMS. This from running and plotting lots of simulations in EGGERS1.
+% SIMULOSL, BLUROSY, BLUROSY_DEMO
 %
 % Maybe should formally increase it in those cases so as to never worry?
 %
-% Last modified by fjsimons-at-alum.mit.edu, 06/20/2018
+% Last modified by fjsimons-at-alum.mit.edu, 10/31/2018
 
 if params.blurs<0
   error('You should be running BLUROSY, not BLUROS!')
@@ -52,6 +46,8 @@ defval('xver',1)
 % Target dimensions, the original ones
 NyNx=params.NyNx;
 dydx=params.dydx;
+% The blurring refinement factor
+blurs=params.blurs;
 
 % The unblurred-property wavenumber grid
 [k,dci,~,kx,ky]=knums(params);
@@ -67,26 +63,22 @@ end
 % Find out what you've actually been given, remember S worked on KNUMS
 NyNx2=[length(ky2) length(kx2)];
 
-% Use the EFFECTIVE blurs for the Fejer kernel!
-blurs=sqrt(prod(NyNx2)/prod(NyNx));
-
-if blurs~=params.blurs && xver==1
-  % Could have been innocuous, from the parity protection, or more major
-  disp(sprintf('Overriding blurs from %4.2f to %4.2f from KNUMS parity',...
-               params.blurs,blurs))
-end
-
 % This is the periodogram of the boxcar for the old grid 
 % interpolated to the new grid so we don't just hit the nodes
 % See http://blinkdagger.com/matlab/matlab-fft-and-zero-padding/
 % Should do this once and save it. Now let's not forget that in our
 % formalism we force the fft/ifft to be unitary
-% If params.blurs were to be 1, we would get a single 1 in the center
+% If blurs were to be 1, we would get a single 1 in the center
 % Note that the kernel values are very different depending on even/odd dimensions
-Fejk=fftshift(abs(fft2(repmat(1/prod(NyNx)/blurs,NyNx),NyNx2(1),NyNx2(2))).^2);
+% Note that we are not using the parameter blurs itself, but the
+% final dimensions, which could have been subject to the parity
+% preservation, and, in the past, necessitated an "effective"
+% blurring, which just got to be too cumbersome
+Fejk=fftshift(abs(fft2(repmat(1/sqrt(prod(NyNx))/sqrt(prod(NyNx2)),NyNx),NyNx2(1),NyNx2(2))).^2);
 
 if xver==1
-  % Make sure it is unitary and norm-preserving
+  % Make sure it is unitary and norm-preserving, that's why we need
+  % the override
   difer(sum(Fejk(:))-1,[],[],NaN)
   % Check Hermiticity of the Fejer kernel, no reason to doubt it
   hermcheck(Fejk)
@@ -105,10 +97,34 @@ if [ky(1)-ky2(1)]<0; ky2(1)=ky(1); end
 Sbar=nan(prod(NyNx),size(S,2));
 % Make sure there are no NaNs in the output
 for in=1:size(S,2)
+  
+  % Is there a simpler way in which the new grid is a superset of
+  % the old grid
+  disp(sprintf('\nINTERPOLATE:'))
+  tic
   % Later, consider griddedInterpolant
   Hh=interp2(kx2(:)',ky2(:),...
              conv2(Fejk,reshape(S(:,in),NyNx2),'same'),...
              kx(:)',ky(:),'cubic');
+  toc
+
+  if round(blurs)==blurs && ...
+          [abs(sum(kx2(1+mod(NyNx2(2),2):blurs:end)-kx))+abs(sum(ky2(1+mod(NyNx2(1),2):blurs:end)-ky))]==0
+    disp(sprintf('\nSUBSAMPLE:'))
+    tic
+    Hhi=conv2(Fejk,reshape(S(:,in),NyNx2),'same');
+    Hhi=Hhi(1+mod(NyNx2(2),2):blurs:end,1+mod(NyNx2(1),2):blurs:end);
+    toc
+    difer([Hh-Hhi]/norm(Hh))
+    % Subsampling will always be better than interpolating, won't it??
+    %    Hh=Hhi;
+  end
+  
+keyboard
+% Need to consider that we can subsample even more, 
+% Need to consider the peak mismatch
+% Need to consider specific values that are problematic, and why
+
   % In the even case only
   if ~mod(NyNx(1),2); Hh(1,:)=Hh(1,:)*2; end
   if ~mod(NyNx(2),2); Hh(:,1)=Hh(:,1)*2; end
