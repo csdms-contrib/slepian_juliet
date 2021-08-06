0dcd51cfb425d954d9abe319de0d2a3b4f0cb7df
Frederik J. Simons <fjsimons@gmail.com>
2021-08-06 06:03:36 -0400

    Calculates the three-parameter isotropic d-dimensions Matern covariance

--- maternosy.m
+++ maternosy.m
@@ -31,6 +31,21 @@ function Cy=maternosy(y,th,varargin)
 % [sum(y.*maternosy(y,th0))*(y(2)-y(1))/(2*pi) maternos(0,th0)]
 %
 % Last modified by fjsimons-at-alum.mit.edu, 11/03/2016
+%
+%% The Fourier relation is impossible to verify... since we never observe
+%% all lags fine enough... and that is the point of estimating it
+%% differently. See the comparison in SIMULOSL1 which is more fruitful.
+% th=[2000 0.5 1];
+% p.NyNx=[4300 5500]; lY=13; lX=24;
+% [k,kx,ky,dci]=knum2(p.NyNx,[lY lX]); p.dydx=[lY/(M-1) lX/(N-1)];
+% x=[-floor(p.NyNx(2)/2):1:+floor(p.NyNx(2)/2)-1]*p.dydx(2);
+% y=[-floor(p.NyNx(1)/2):1:+floor(p.NyNx(1)/2)-1]*p.dydx(1);
+% [X,Y]=meshgrid(x,y); yy=sqrt(X.^2+Y.^2);
+% Sb=maternos(k,th,2); Sbb=v2s(Sb,p);
+% Cy=maternosy(yy,th,2); difer(Cy(dci(1),dci(2))-th(1))
+% Sk=tospace(Cy,p); Skk=fftshift(v2s(Sk,p));
+% plot(log10(Sbb(dci(1),:))); hold on ; plot(log10(abs(Skk(dci(1),:))));
+% hold off
 
 % These are always the last three elements of the input 
 s2=th(end-2);
