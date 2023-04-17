ffd619c9e9b15cddebac93046e6cbb3362cebc82
Frederik J. Simons <fjsimons@gmail.com>
2023-04-17 17:44:37 -0400

    Calculates the blurred isotropic d-dimensions Matern spectral density

--- maternosp.m
+++ maternosp.m
@@ -14,10 +14,16 @@ function [Sk,k]=maternosp(th,params,xver)
 % params   A structure with AT LEAST these constants that are known:
 %          dydx  sampling interval in the y and x directions [m m]
 %          NyNx  number of samples in the y and x directions
-%          blurs 0 Don't blur likelihood using the Fejer window
-%                N Blur likelihood using the Fejer window [default: N=2]
-%               -1 Blur likelihood using the exact procedure
-% xver     Perform excessive verification [0 or 1]          
+%          blurs 0 No wavenumber blurring
+%                1 No wavenumber blurring, effectively
+%                N Fejer convolutional  BLUROS  on an N-times refined grid
+%               -1 Fejer multiplicative BLUROSY using the exact procedure
+%              Inf Rather simulate using SGP invariant embedding, error
+%           taper  0 there is no taper near of far
+%                  1 it's a unit taper, implicitly
+%                  OR an appropriately sized taper with proper values 
+%                    (1 is yes and 0 is no and everything in between)
+% xver     Perform excessive verification [0 or 1]
 %
 % OUTPUT:
 %
@@ -28,7 +34,7 @@ function [Sk,k]=maternosp(th,params,xver)
 %
 % MATERNOS, MATERNOSY, MATERNPRC, KNUMS, BLUROS, BLUROSY, BLURCHECK
 %
-% Last modified by fjsimons-at-alum.mit.edu, 11/02/2018
+% Last modified by fjsimons-at-alum.mit.edu, 03/07/2023
 
 % Default is to oververify it all
 defval('xver','1')
@@ -41,29 +47,33 @@ blurs=params.blurs;
 
 %  Calculate the Matern spectrum for the spectral parameters
 switch blurs
- case {0,1}
-  % The unblurred-property wavenumber grid
-  k=knums(params); k=k(:);
-  % disp(sprintf('%s without blurring',upper(mfilename)))
-  Sk=maternos(k,th,d);
- otherwise
-  % disp(sprintf('%s with blurring factor %i',upper(mfilename),blurs))
-  if blurs>1
-    % If I stay on the same k-grid, I'm really not doing any convolution at
-    % all as you can see quickly. So by "suitably discretizing" the
-    % convolutional operator we mean: performing it on a highly densified
-    % grid of which the target grid may be a subset. Doing this on the
-    % same grid would be the "inverse crime" of NOT changing the grid
-    % all. Run Fk for this case to see it then would be a delta function.
+  case {0,1}
+    % The unblurred-property wavenumber grid
+    k=knums(params); k=k(:);
+    % disp(sprintf('%s without blurring',upper(mfilename)))
+    Sk=maternos(k,th,d);
+  otherwise
+    if isinf(blurs)
+        error('You should be calling MATERNOSY, not MATERNOSP!')
+    end
+    if blurs>1 & ~isinf(blurs)
+        % disp(sprintf('%s with blurring factor %i',upper(mfilename),blurs))
 
-    % Blurs IS the refinement parameter; make new wavenumber grid. Make the
-    % spectral-spectral portion of the spectral matrix, convolve in two
-    % dimensions, and then subsample onto the original target grid
-    [Sk,k]=bluros(maternos(knums(params,1),th),params,xver);
-  else
-    % Here is the alternative EXACT way of doing it, which does away
-    % with the approximate convolutional refinement procedure
-    % disp(sprintf('%s with exact blurring',upper(mfilename)))
-    [Sk,k]=blurosy(th,params,xver);
-  end
+        % If I stay on the same k-grid, I'm really not doing any convolution at
+        % all as you can see quickly. So by "suitably discretizing" the
+        % convolutional operator we mean: performing it on a highly densified
+        % grid of which the target grid may be a subset. Doing this on the
+        % same grid would be the "inverse crime" of NOT changing the grid
+        % all. Run Fk for this case to see it then would be a delta function.
+        
+        % Blurs IS the refinement parameter; make new wavenumber grid. Make the
+        % spectral-spectral portion of the spectral matrix, convolve in two
+        % dimensions, and then subsample onto the original target grid
+        [Sk,k]=bluros(maternos(knums(params,1),th),params,xver);
+    else
+        % Here is the alternative EXACT way of doing it, which does away
+        % with the approximate convolutional refinement procedure
+        % disp(sprintf('%s with exact blurring',upper(mfilename)))
+        [Sk,k]=blurosy(th,params,xver);
+    end
 end
