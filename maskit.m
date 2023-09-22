762c3d8948974b642bd2edee6cd35dfcdf75c5df
Frederik J. Simons <fjsimons@gmail.com>
2023-09-22 13:39:43 -0400

    Masks (and merges) an unwrapped image matrix (set)

--- maskit.m
+++ maskit.m
@@ -6,7 +6,7 @@ function varargout=maskit(v,p,scl,w)
 % v       A vector that is the unwrapping of a matrix
 % p       A single parameter structure with AT LEAST this:
 %           NyNx  number of samples in the y and x directions
-%           taper an index matrix with a mask, OR:
+%           mask an index matrix with a mask, OR:
 %                 a region name that will be scaled to fit
 % scl     A scale between 0 and 1 with the occupying center
 %         fraction of the domain enclosed by the input curve; due
@@ -18,16 +18,17 @@ function varargout=maskit(v,p,scl,w)
 %
 % v       The masked output matrix, unrolled into a vector
 % cr      The colum,row index coordinates of the masking curve
-% I       The mask, unrolled into a vector
+% I       The mask, unrolled into a vector, note, this is an "anti-taper"
 % w       The second masked output matrix, unrolled into a vector
 % vw      The merged field, second field w sucked into first field v
 %
 % EXAMPLE:
 %
-% maskit('demo1') % A geographical region masking a single field
-% maskit('demo2') % A geographical region merging two fields
-% maskit('demo1','england') % Etc for more geographical variability
 % [~,~,I]=maskit('demo1'); % To get a quick mask to look at
+% maskit('demo1') % A geographical region masking a single field
+% maskit('demo1','england') % 'amazon', 'orinico', for geographical variability
+% maskit('demo2') % A geographical region merging two fields
+% maskit('demo2','england') % 'amazon', 'orinico', for geographical variability
 %
 % Last modified by fjsimons-at-alum.mit.edu, 09/21/2023
 
@@ -36,8 +37,8 @@ defval('v','demo1')
 
 if ~isstr(v)
     % Get the mask by name
-    if isstr(p.taper)
-        XY=eval(p.taper);
+    if isstr(p.mask)
+        XY=eval(p.mask);
 
         % Default scale is 80% of the area
         defval('scl',0.8)
@@ -54,7 +55,7 @@ if ~isstr(v)
     else
         % You already have it
         cr=NaN;
-        I=p.taper;
+        I=p.mask;
     end
 
     % Apply the mask to v, w and merge into vw as desired
@@ -76,60 +77,62 @@ elseif strcmp(v,'demo1')
     defval('p','france')
     defp=p; clear p
     % Now proceed with a fresh copy
-    p.taper=defp;
+    p.mask=defp;
     p.quart=0; p.blurs=Inf; p.kiso=NaN; clc; 
     [Hx,th,p]=simulosl([],p,1);
     [Hm,cr,I]=maskit(Hx,p);
+    clf
     subplot(121); plotit(Hx,p,cr,th)
     subplot(122); plotit(Hm,p,cr,[])
-    % figdisp(sprintf('maskit_%s',p.taper),[],[],2)
     v=Hm; [w,vw]=deal(NaN);
 elseif strcmp(v,'demo2')
     % Capture the second input
     defval('p','france')
     defp=p; clear p
     % Now proceed with a fresh copy
-    p.taper=defp;
+    p.mask=defp;
     % Simulate using invariant embedding, no taper
     p.quart=0; p.blurs=Inf; p.kiso=NaN; clc;
     % Something manageable without overdoing it
-    p.NyNx=[188 233];
+    p.NyNx=[188 233]+randi(20,[1 2]);
 
-    N=3;
+    warning('Not ready yet')
+    doit=0;
+
+    N=1;
     for index=1:N
         % Simulate first field with defaults from simulosl
         [Hx,th,p]=simulosl([],p,1);
         % Simulate second field by making a small change
         th2=th; th2(2)=2.5; th2(3)=30000;
         [Gx,th2,p]=simulosl(th2,p,1);
+
         % Now do the masking and the merging
         [Hm,cr,I,Gm,HG]=maskit(Hx,p,[],Gx);
-        % No merging?
-        % [Hm,cr,I,Gm,HG]=maskit(Hx,p,[],Hx);
-        subplot(221); plotit(Hx,p,cr,th)
-        subplot(223); plotit(Gx,p,cr,th2)
+        % No merging? [Hm,cr,I,Gm,HG]=maskit(Hx,p,[],Hx);
+        clf
+        ah(1)=subplot(221); plotit(Hx,p,cr,th)
+        ah(3)=subplot(223); plotit(Gx,p,cr,th2)
         ah(4)=subplot(224); plotit(HG,p,cr,[])
         movev(ah(4),0.25)
-        % figdisp(sprintf('maskit4_%s',p.taper),[],[],2)
         v=Hm; w=Gm; vw=HG;
+
         % Now recover the parameters of HG but only in the region I
         p.taper=I;
-
-        keyboard
-        % Not ready yet
-        
-        % No masking?
-        % p.taper=1;
-        % As appropriate you'll force the use of BLUROSY in MATERNOSP in LOGLIOS
-        p.blurs=-1;
-        % Perform the optimization on the insert
-        try
-            [thhat(index,:),~,~,scl(index,:)]=mleosl(HG,[],p,[],[],[],[]);
-        end
-        % Perform the optimization on the ~insert
-        p.taper=~I;
-        try
-            [thhat2(index,:),~,~,scl2(index,:)]=mleosl(HG,[],p,[],[],[],[]);
+        if doit==1
+            % No masking?
+            % p.mask=1;
+            % As appropriate you'll force the use of BLUROSY in MATERNOSP in LOGLIOS
+            p.blurs=-1;
+            % Perform the optimization on the insert
+            try
+                [thhat(index,:),~,~,scl(index,:)]=mleosl(HG,[],p,[],[],[],[]);
+            end
+            % Perform the optimization on the ~insert
+            p.taper=~I;
+            try
+                [thhat2(index,:),~,~,scl2(index,:)]=mleosl(HG,[],p,[],[],[],[]);
+            end
         end
     end
 end
