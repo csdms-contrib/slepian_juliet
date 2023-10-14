5ba81ae424e6bce44ebc7f6e591e676e7fa610c4
Frederik J. Simons <fjsimons@gmail.com>
2023-10-14 16:46:36 -0400

    Expected value of the second derivative of the log-likelihood function

--- fishiosl.m
+++ fishiosl.m
@@ -37,7 +37,7 @@ function [F,covF,cF]=fishiosl(k,th,xver,params)
 % [L,Lg,LH]=logliosl(k,th0,1,p,Hk);
 % difer(Lg-g); difer(LH-H); % should be passing the test
 %
-% Last modified by fjsimons-at-alum.mit.edu, 08/12/2021
+% Last modified by fjsimons-at-alum.mit.edu, 10/14/2023
 
 % Early setup exactly as in HESSIOSL
 defval('xver',1)
@@ -64,17 +64,15 @@ cF=nan(npp,1);
 
 % We're abusing the 'xver' switch to bypass saving wavenumber-dependencies
 if xver==0
-  % Attempt to do this otherwise
+  % Attempt to do this otherwise for variance calculation
   % FJS [~,kz,W]=blurosy(th,params);
-
   % Do not save the wavenumber-dependent entities
   for ind=1:npp
     % Eq. (A60) in doi: 10.1093/gji/ggt056
     cF(ind)=mean(mth{i(ind)}.*mth{j(ind)});
-    % Attempt to do this otherwise
-    % FJS cFb(ind)=indeks(bsxfun(@times,mth{i(ind)},mth{j(ind)}'),':').*W(~~kz));
+    % Attempt to do this otherwise for variance calculation
+    % FJS cFb(ind)=indeks(bsxfun(@times,mth{i(ind)},mth{j(ind)}'),':').*W(~~kz);
   end
-  % FJS keyboard
 elseif xver==1
   % Initialize; some of them depend on the wave vectors, some don't
   cFk=cellnan([npp 1],[1 repmat(lk,1,5)],repmat(1,1,6));
