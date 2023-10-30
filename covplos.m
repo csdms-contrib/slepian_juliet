72faccb3cffe01dd0022442b70f6ee6b95577aaf
Frederik J. Simons <fjsimons@gmail.com>
2023-10-30 15:33:38 -0400

    MLE results covariance plotting routine

--- covplos.m
+++ covplos.m
@@ -1,24 +1,16 @@
-function varargout=covplos(oneortwo,sclcovX,obscov,covX,params,thhats,th0,E,v,cblox)
-% COVPLOS(oneortwo,sclcovX,obscov,covX,params,thhats,th0,E,v,cblox)
+function varargout=covplos(oneortwo,sclcovX,obscov,params,thhats,E,v,cblox)
+% COVPLOS(oneortwo,sclcovX,obscov,params,thhats,E,v,cblox)
 %
-% Makes a nice covariance plot of the MLE results
+% Makes a nice covariance plot of the maximum-likelihood estimation results
 %
 % INPUT:
 %
 % oneortwo  1 Plots only prediction
 %           2 Plots prediction and observation [default]
-% sclcovX   The scaled predicted covariance matrix
-% obscov    The scaled observed covariance matrix
-% covX      The predicted covariance matrix before scaling (used for the title)
+% sclcovX   The scaled covariance estimate
+% obscov    The scaled sample covariance matrix
 % params    The parameters from the experiment, for the title only
 % thhats    The estimated model parameter vector, whose length we need
-% th0       The true model parameter vector
-%           th0(1)=D    Isotropic flexural rigidity 
-%           th0(2)=f2   The sub-surface to surface initial loading ratio 
-%           th0(3)=r    The sub-surface to surface initial correlation coefficient
-%           th0(1/4)=s2   The first Matern parameter, aka sigma^2 
-%           th0(2/5)=nu   The second Matern parameter 
-%           th0(3/6)=rho  The third Matern parameter 
 % E         Young's modulus (not used for single fields and only for title)
 % v         Poisson's ratio (not used for single fields and only for title)
 % cblox     Is the colorbar 'hor' or 'ver' [default]
@@ -27,7 +19,7 @@ function varargout=covplos(oneortwo,sclcovX,obscov,covX,params,thhats,th0,E,v,cb
 %
 % ah,yl     Axis handles to what was just plotted
 %
-% Last modified by fjsimons-at-alum.mit.edu, 06/20/2018
+% Last modified by fjsimons-at-alum.mit.edu, 10/30/2023
 
 defval('oneortwo',2)
 defval('cblox','ver')
@@ -94,18 +86,20 @@ end
 t=ostitle(ah,params,[],length(thhats(:,1)));
 movev(t,.5)
 % We are quoting the TRUTHS and the 1xstandard deviation based on COVX
-[answ,answs]=osansw(th0,covX,E,v);
-
-tt=supertit(ah,sprintf(answs,answ{:}));
+% Here is the MEAN ESTIMATE and its OBSERVED-COVARIANCE-based standard deviation
+[answ,answs]=osansw(mean(thhats),cov(thhats),E,v);
+disp(sprintf('\n%s',...
+             'Mean estimate and ensemble-covariance standard deviation'))
+disp(sprintf(answs,answ{:}))
+% By the way, use THAT as a title
+tt=supertit(ah(1:2),sprintf(answs,answ{:}));
 movev(tt,-5)
-
-moveh(tt,-0.075)
 
 % Output
 varns={ah,yl};
 varargout=varns(1:nargout);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function covannotate(ah,np)
 
 if np==6
