e10685ef1467b49d3b0b5d2794d842db306e4f35
Frederik J. Simons <fjsimons@gmail.com>
2023-12-18 14:51:49 -0500

    Calculates the isotropic d-dimensional Matern covariance

--- maternosy.m
+++ maternosy.m
@@ -18,6 +18,9 @@ function varargout=maternosy(y,th,varargin)
 % OUTPUT:
 %
 % Cy       The spatial Matern covariance at all the requested lags
+% Cy2      The spatial Matern covariance calculated using the Abramowitz &
+%          Stegun (1965) simplifications to the Bessel function for half-
+%          integer values of nu; Cy2 should be equivalent to Cy
 %
 % SEE ALSO:
 %
@@ -29,26 +32,83 @@ function varargout=maternosy(y,th,varargin)
 %
 % [Hx,th0,p]=simulosl; bigN=1000;
 % y=linspace(0,sqrt(prod(p.dydx))*sqrt(prod(p.NyNx)),bigN);
+% S(0)=1/(2*pi)\int C(r)rdr
 % [sum(y.*maternosy(y,th0))*(y(2)-y(1))/(2*pi) maternos(0,th0)]
 %
+% nu=[1/3 1/2 1 3/2 5/2]; th0(2)=nu(randi(length(nu))); th0(2)
+% [Cy,Cy2]=maternosy(y,th0);
+%
 % maternosy('demo1')
-% 
-% Last modified by fjsimons-at-alum.mit.edu, 02/23/2023
+%
+% Last modified by fjsimons-at-alum.mit.edu, 12/18/2023
+% Last modified by olwalbert-at-princeton.edu, 12/18/2023
 
 if ~isstr(y)
     % These are always the last three elements of the input 
     s2=th(end-2);
     nu=th(end-1);
     rh=th(end  );
-    
     % The argument, make sure it is a distance
     argu=2*sqrt(nu)/pi/rh*abs(y);
     % The evaluation
     Cy=2^(1-nu)*s2/gamma(nu)*argu.^nu.*besselk(nu,argu);
     % Supply the smallest arguments
     Cy(y==0)=s2;
-    % Optional output
-    varns={Cy};
+    % Probably in SIMULOSL we would add Cy(y==0)=s2*1.025;
+    % If the use of the nugget a la Arthur is desired, uncomment the
+    % following. Arthur says there is no magical value for the nugget, but
+    % uses 0.025 in a shared example.
+    % nugget_effect = s2*0.025.*(y==0);
+    % Cy+=nugget_effect;
+    if nargout>1
+        % If the number of outputs requested is greater than 1, we
+        % are seeking to evaluate Cy2 from the simplified analytic
+        % expression of the isotropic Matern covariance for a special 
+        % value of nu. The following analytic forms of half-integer
+        % nu are calculated from substitution of Eqs 10.1.9 and 10.2.15
+        % of Abramowitz & Stegun (1965) for modified
+        % Bessel functions of half-integer orders.
+        % See also 10.1093/biomet/93.4.989
+        if nu==1/3
+            % von Karman 
+            Cy2=s2*2^(2/3)/gamma(nu).*(2*sqrt(3)/(3*pi*rh)*abs(y)).^...
+                (nu).*besselk(nu,2*sqrt(3)/(3*pi*rh)*abs(y));
+            % Compute the value at zero lag
+            Cy2(y==0)=s2;
+        elseif nu==1/2
+            % Exponential
+            Cy2=s2*exp(-sqrt(2)/(pi*rh)*abs(y));
+        elseif nu==1
+            % Whittle
+            Cy2=s2*2/(pi*rh)*abs(y).*besselk(nu,2/(pi*rh)*abs(y));
+            % Compute the value at zero lag
+            Cy2(y==0)=s2;
+        elseif nu==3/2
+            % Second-order autoregressive
+            Cy2=s2*exp(-sqrt(6)/(pi*rh)*abs(y)).*(1+sqrt(6)/(pi*rh)*abs(y));
+        elseif nu==5/2
+            % Third-order autoregressive
+            Cy2=s2*exp(-sqrt(10)/(pi*rh)*abs(y)).*(1+sqrt(10)/(pi*rh)*...
+                abs(y)+10/(3*pi^2*rh^2)*abs(y).^2);            
+        else
+            % However, if the nu provided to MATERNOSY is not one of the
+            % five special values of nu, we should throw an error
+            error('This is not a special case of nu. Request a single output.')        
+        end
+        if isinf(nu)
+            Cy2=s2*exp(-y^2/(2*pi^2*rh^2));
+        end
+        % Uncomment if using the nugget effect:
+        % Cy2+=nugget_effect;
+        % If we calculated Cy2 for a special case of nu, we should 
+        % compare it to Cy
+        difer(Cy/(Cy(1))-Cy2/Cy2(1))
+        % Make both Cy and Cy2 available as output
+        varns={Cy,Cy2};
+    else
+        % Optional output
+        varns={Cy};
+    end
     varargout=varns(1:nargout);
 elseif strcmp(y,'demo1')
     % The Fourier relation between the correlation and the covariance
@@ -85,6 +145,7 @@ elseif strcmp(y,'demo1')
     hold on
     plot(log10(Sbb(dci(1),:))-log10(m),'LineWidth',2,'Color','m');
     hold off
+    legend('FFT(MATERNOSY)','MATERNOS')
     axis tight
     grid on
     % Annotate 
