9f26d9237072a40f1dd0e96ed0e793bdbd325107
Frederik J. Simons <fjsimons@gmail.com>
2024-06-03 11:43:13 -0400

    Calculates the isotropic d-dimensional Matern spectral density

--- maternos.m
+++ maternos.m
@@ -14,11 +14,11 @@ function varargout=maternos(k,th,dth,d,meth)
 %          s2   The first Matern parameter [variance in units^2]
 %          nu   The second Matern parameter [differentiability]
 %          rho  The third Matern parameter [range in m]
+% dth      When empty, the spectral density is calculated; otherwise,
+%          1, 2, or 3 specifies which element of th gets differentiated
 % d        The dimensionality [default is 2]
 % meth     1 straight up calculation
 %          2 analytical simplification for special values of th(end-1)
-% dth      When empty, the spectral density is calculated; otherwise,
-%          1, 2, or 3 specifies which element of th gets differentiated
 %
 % OUTPUT:
 %
@@ -38,12 +38,12 @@ function varargout=maternos(k,th,dth,d,meth)
 % Periodogram for the inifinite smoothness, Gaussian, squared exponential case:
 %
 % th0(2)=Inf; k=knums(p); k=k(:); Sk=maternos(k,th0);
-% figure(); imagesc(log10(v2s(Sk,p))); axis image
+% imagesc(log10(v2s(Sk,p))); axis image
 %
 % Confirmation that the special case simplifications are equivalent to the
 % straight calculation:
 %
-% nus=[1/3,1,1/2,3/2,5/2,7/2,15/2];th=[1 nus(randi(length(nus),1)) 1]
+% nus=[1/3,1,1/2,3/2,5/2,7/2,15/2]; th=[1 nus(randi(length(nus),1)) 1]
 % Sk=maternos(k,th,[],2,1); Sk2=maternos(k,th,[],2,2); all(Sk==Sk2)
 %
 % Calculate and visualize the partial derivatives: 
@@ -71,6 +71,8 @@ switch d
  otherwise
   pd=gamma(nu+d/2)/gamma(nu);
 end
+
+keyboard
 
 % Calculate the spectral density?
 if isempty(dth)
@@ -100,16 +102,16 @@ if isempty(dth)
               if nu==1/3
                   % von Karman
                   if d==2
-                    Sk=s2*2^(2/3)*pi*rh^2/(4+3*(pi*rh*k(:)).^2).^(4/3); 
+                    Sk=s2*2^(2/3)*pi*rh^2./(4+3*(pi*rh*k(:)).^2).^(4/3); 
                   else
                     Sk=gamma(1/3+d/2)/gamma(1/3)*s2*...
-                        (4/(4+3*(pi*rh*k(:)).^2)).^(1/3)*...
-                        (3*pi*rh^2/(4+3*(pi*rh*k(:)).^2))^(d/2);
+                        (4./(4+3*(pi*rh*k(:)).^2)).^(1/3).*...
+                        (3*pi*rh^2./(4+3*(pi*rh*k(:)).^2)).^(d/2);
                   end
               elseif nu==1/2
                   % Exponential
                   if d==2
-                      Sk=s2*pi*rh^2/(sqrt(2)*(2+(pi*rh*k(:)).^2).^(3/2));
+                      Sk=s2*pi*rh^2./(sqrt(2)*(2+(pi*rh*k(:)).^2).^(3/2));
                   else
                       Sk=gamma((d+1)/2)*s2*(2/(pi^(1-d)))^(1/2)*rh^d*...
                           (2+(pi*rh*k(:)).^2).^(-(d+1)/2);
@@ -117,23 +119,23 @@ if isempty(dth)
               elseif nu==1
                   % Whittle
                   if d==2
-                    Sk=s2*4*pi*rh^2/(4+(pi*rh*k(:)).^2).^2; 
+                    Sk=s2*4*pi*rh^2./(4+(pi*rh*k(:)).^2).^2; 
                   else
                     Sk=gamma(1+d/2)*s2/pi^(d/2)*(2/(pi*rh))^2*...
-                        ((pi*rh)^2/(4+(pi*rh*k(:)).^2)).^(1+d/2);
+                        ((pi*rh)^2./(4+(pi*rh*k(:)).^2)).^(1+d/2);
                   end
               elseif nu==3/2
                   % Second-order autoregressive
                   if d==2
-                    Sk=9*sqrt(6)*s2*pi*rh^2/(6+(pi*rh*k(:)).^2).^(5/2);
+                    Sk=9*sqrt(6)*s2*pi*rh^2./(6+(pi*rh*k(:)).^2).^(5/2);
                   else
-                    Sk=gamma((d+3)/2)*12*(6*pi)^(1/2)*s2*rh^2/...
+                    Sk=gamma((d+3)/2)*12*(6*pi)^(1/2)*s2*rh^2./...
                         (6+(pi*rh*k(:)).^2).^(5/2);
                   end
               elseif nu==5/2
                   % Third-order autoregressive
                   if d==2
-                    Sk=250*sqrt(10)*s2*pi*rh^2/(10+(pi*rh*k(:)).^2).^(7/2);
+                    Sk=250*sqrt(10)*s2*pi*rh^2./(10+(pi*rh*k(:)).^2).^(7/2);
                   else
                     Sk=gamma((d+5)/2)*s2*400/3*(10)^(1/2)*pi^(d/2-1/2)*rh^d*...
                         (10+(pi*rh*k(:)).^2).^(-(d+5)/2);
@@ -143,10 +145,10 @@ if isempty(dth)
                   % nu=n+1/2 for n = 1, 2, 3, ...
                   n=nu-1/2;
                   if d==2
-                    Sk=s2*pi*rh^2/4*((4*n+2)/(4*n+2+(pi*rh*k(:)).^2)).^(n+3/2);
+                    Sk=s2*pi*rh^2/4*((4*n+2)./(4*n+2+(pi*rh*k(:)).^2)).^(n+3/2);
                   else
-                    Sk=gamma(n+(d+1)/2)/gamma(n+1/2)*s2*((4*n+2)/...
-                        (4*n+2+(pi*rh*k(:)).^2)).^(n+1/2)*...
+                    Sk=gamma(n+(d+1)/2)/gamma(n+1/2)*s2*((4*n+2)./...
+                        (4*n+2+(pi*rh*k(:)).^2)).^(n+1/2).*...
                         (pi*rh^2/(4*n+2+(pi*rh*k(:)).^2)).^(d/2);
                   end
               else
@@ -171,11 +173,11 @@ else
        % Gamma function and 
        Sk=s2*pd*nu^nu*4^nu/pi^(d/2)/(pi*rh)^(2*nu).*avark.^(-nu-d/2)*...
           (psi(nu+d/2)-psi(nu)+log(nu*4/(pi*rh)^(2))-log(avark)-...
-          (4*nu+2*d)/(4*nu+(pi*rh*k(:)).^2)+1);
+          (4*nu+2*d)./(4*nu+(pi*rh*k(:)).^2)+1);
    elseif dth==3
        % The partial derivative of Sk with respect to the range, dSkdrh
        Sk=s2*pd*nu^nu*4^nu/pi^(d/2)/(pi*rh)^(2*nu).*avark.^(-nu-d/2-1)*...
-          (2*nu/rh)*(2*d/(pi*rh)^2-k(:).^2);
+          (2*nu/rh).*(2*d/(pi*rh)^2-k(:).^2);
    else
        error('Not a valid partial derivative index. Ask for dth=1,2,or 3.') 
    end
