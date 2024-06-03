40f55579bcb97ae12dbfbeb92ad09c839b63d69d
Frederik J. Simons <fjsimons@gmail.com>
2024-06-03 10:51:07 -0400

    Calculates the isotropic d-dimensional Matern spectral density

--- maternos.m
+++ maternos.m
@@ -23,9 +23,13 @@ function Sk=maternos(k,th,varargin)
 %
 % EXAMPLE:
 %
-% [~,th0]=simulosl; difer(maternos(0,th0)/th0(1)-pi*th0(3)^2/4,7,[],NaN)
+% [~,th0,p]=simulosl; difer(maternos(0,th0)/th0(1)-pi*th0(3)^2/4,7,[],NaN)
+%
+% th0(2)=Inf; k=knums(p); k=k(:); Sk=maternos(k,th0);
+% figure(); imagesc(log10(v2s(Sk,p))); axis image
 %
 % Last modified by fjsimons-at-alum.mit.edu, 12/18/2023
+% Last modified by olwalbert-at-princeton.edu, 12/22/2023
 
 % These are always the last three elements of the input 
 s2=th(end-2);
@@ -39,10 +43,6 @@ else
   d=2;
 end
 
-if isinf(nu)
-    warning('Build in special case from Fourier transforming asymptotic exponential from MATERNOSY')
-end
-
 % Adjust for dimensionality by specialization
 switch d
  case 2
@@ -51,7 +51,17 @@ switch d
   pd=gamma(nu+d/2)/gamma(nu);
 end
 
-% Calculate the denominator in the spectral density
-avark=4*nu/pi^2/rh^2+k(:).^2;
-% Calculate the d-dimensional spectral density
-Sk=s2*pd*nu^nu*4^nu/pi^(d/2)/(pi*rh)^(2*nu).*avark.^(-nu-d/2);
+% Check whether we are calculating the spectral density for the case that
+% nu approaches infinity or for the general case
+if isinf(nu)
+    % Calculate the d-dimensional squared exponential spectral density 
+    % This analytic form is solved for from the general case of the
+    % d-dimensional spectral density using Eq. 6.1.46 of Abramowitz &
+    % Stegun (1965) and L'Hopital's rule
+    Sk=s2*(pi*rh^2/4)^(d/2)*exp(-pi^2*rh^2*k(:).^2/4);
+else
+    % Calculate the denominator in the spectral density
+    avark=4*nu/pi^2/rh^2+k(:).^2;
+    % Calculate the d-dimensional spectral density
+    Sk=s2*pd*nu^nu*4^nu/pi^(d/2)/(pi*rh)^(2*nu).*avark.^(-nu-d/2);
+end
