8cb05de96d4db485c7f7881292e05a6673282782
Frederik J. Simons <fjsimons@gmail.com>
2025-04-08 10:50:21 -0400

    A matrix slicer depending on which parameters were inverted for

--- matslice.m
+++ matslice.m
@@ -1,14 +1,14 @@
-function B=matslice(A,ifinv,r)
-% B=matslice(A,IFINV,R)
+function B=matslice(A,keepdim,r)
+% B=matslice(A,KEEPDIM,R)
 %
-% Slices the input matrix to remove the row(s) and column(s) that pertain to a
-% fixed (not inverted) value. 
+% Remove row(s) and column(s) etc from an array
 %
 % INPUTS:
 %
-% A     the full matrix
-% ifinv the logical flags for inverted parameters; slicing will occur for 0's
-% r     -1 reverse the slice; introduce row(s) and col(s) of NaNs
+% A         the full matrix
+% keepdim   the logical flags for kept dimensions parameters;
+%           removal will happen for 0's
+% r     -1  reverse the slice; introduce row(s) and col(s) of NaNs
 %
 % OUTPUTS:
 %
@@ -16,40 +16,44 @@ function B=matslice(A,ifinv,r)
 %
 % SEE ALSO:
 % 
-% varianceofestimates, mleosl, matslice
+% VARIANCEOFESTIMATES, MLEOSL
 %
 % EXAMPLE:
 %
-% A=rand(3); ifinv=[1 0 1];
-% B=matslice(A,ifinv);
+% A=rand(3); keepdim=[1 0 1];
+% B=matslice(A,keepdim);
 % disp(sprintf('Size check: %s',string(all(size(B)==[2,2]))))
 % 
 % A=rand(3,1);
-% B=matslice(A,ifinv);
+% B=matslice(A,keepdim);
 % disp(sprintf('Size check: %s',string(all(size(B)==[2,1]))))
 %
 % A=rand(1,3);
-% B=matslice(A,ifinv);
+% B=matslice(A,keepdim);
 % disp(sprintf('Size check: %s',string(all(size(B)==[1,2]))))
 % C=B;
-% B=matslice(C,ifinv,-1);
+% B=matslice(C,keepdim,-1);
 % disp(sprintf('Size check: %s',string(all(size(B)==size(A)))))
 %
-% Last modified by olwalbert-at-princeton.edu, 01/28/2025
+% matslice(matslice(randn(3,3),[1 0 1]),[1 0 1],-1)
+%
+% Last modified by fjsimons-at-princeton.edu, 04/08/2025
+% Last modified by olwalbert-at-princeton.edu, 04/08/2025
 
 % Create a cell array to match the dimensions of A with logicals as elements 
 % for indexing
 defval('r',1)
 
-idx=repmat({logical(ifinv)},sum(size(A)>1),1);
+idx=repmat({logical(keepdim)},sum(size(A)>1),1);
 
 if r==1
   % How to 'index' rows and columns etc all at the same time 
   B=A(idx{:});
 elseif r==-1
-  np=size(ifinv,2);
+  np=size(keepdim,2);
   dm=sum(size(A)>1);
   B=squeeze(reshape(zeros(1,np.^dm),[1 repelem(np,dm)]));
-  % commenting this out to prevent offending trilos(): B(:)=deal(NaN);
+  % Would be nice to get NaNs for blanks, but TRILOS doesn't like it
+  % B(:)=deal(NaN);
   B(idx{:})=A;
 end
